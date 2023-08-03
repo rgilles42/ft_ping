9a949f81040d1088ba4e97367763c201b078f8bf
Raphaël Gilles <rgilles@student.42.fr>
2023-08-03 18:45:05 +0200

    sends actual ping packets

--- includes/ft_ping.h
+++ includes/ft_ping.h
@@ -6,15 +6,43 @@
 /*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 22:16:31 by rgilles           #+#    #+#             */
-/*   Updated: 2023/07/26 22:48:19 by rgilles          ###   ########.fr       */
+/*   Updated: 2023/08/03 16:44:55 by rgilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# include <stdio.h>
+# include <sys/socket.h>
+# include <arpa/inet.h>
+# include <netinet/ip.h>
+# include <netinet/ip_icmp.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/time.h>
+# include <libft.h>
+
+void			*ft_memcpy(void *s, const void *src, size_t n);
+void			ft_bzero(void *s, size_t n);
+
+# define PING_PKT_SIZE		84
+# define PING_PKT_DATA_SIZE	(PING_PKT_SIZE - sizeof(struct icmp) - sizeof(time_t))
+# define SWAP_ENDIANNESS_16(x) (x << 8 | x >> 8);
+# define SWAP_ENDIANNESS_64(x) \
+	( x >> 56 \
+	| (x & (0xFFull << 48 )) >> 40 \
+	| (x & (0xFFull << 40 )) >> 24 \
+	| (x & (0xFFull << 32 )) >> 8 \
+	| (x & (0xFFull << 24 )) << 8 \
+	| (x & (0xFFull << 16 )) << 24 \
+	| (x & (0xFFull << 8 )) << 40 \
+	| (x & (0xFFull << 0 )) << 56 )
+
 typedef struct	s_curping {
 	char*	hostname;
 }				t_curping;
+
+void	parse_command(int argc, char** argv, t_curping* current_command);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,13 +6,59 @@
 /*   By: rgilles <rgilles@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/26 22:14:34 by rgilles           #+#    #+#             */
-/*   Updated: 2023/07/26 22:21:48 by rgilles          ###   ########.fr       */
+/*   Updated: 2023/08/03 17:01:20 by rgilles          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <ft_ping.h>
 
+uint16_t	checksum(void *addr, int size) {
+	uint16_t	*buff;
+	uint32_t	sum;
+
+	buff = (uint16_t *)addr;
+	for (sum = 0; size > 1; size -= 2)
+		sum += *buff++;
+	if (size == 1)
+		sum += *(uint8_t*)buff;
+	sum = (sum >> 16) + (sum & 0xFFFF);
+	sum += (sum >> 16);
+	return (~sum);
+}
+
 int	main(void) {
-	printf("%s\n", "Hello");
+	int					sock_fd;
+	//t_curping			current_ping;
+	struct icmp			icmp_frame;
+	struct sockaddr_in	ping_sockaddr;
+	struct timeval		timestamp;
+
+	ft_bzero(&icmp_frame, sizeof(icmp_frame));
+
+	//parse_command(argc, argv, &current_ping);
+	gettimeofday(&timestamp, NULL);
+
+	inet_pton(AF_INET, "8.8.8.8", &ping_sockaddr.sin_addr);
+	ping_sockaddr.sin_family = AF_INET;
+	ping_sockaddr.sin_port = 0;
+
+
+	icmp_frame.icmp_type = ICMP_ECHO;
+	icmp_frame.icmp_code = 0;
+	icmp_frame.icmp_id = SWAP_ENDIANNESS_16((uint16_t)getpid());
+	icmp_frame.icmp_seq = 0;
+	ft_memcpy(&icmp_frame.icmp_dun, &timestamp.tv_sec, sizeof(timestamp.tv_sec));
+	ft_memcpy(&icmp_frame.icmp_dun + sizeof(timestamp.tv_sec), "googoogaga", 11);
+
+	icmp_frame.icmp_cksum = checksum(&icmp_frame, sizeof(icmp_frame));
+	printf("%lu\n", sizeof(timestamp.tv_sec));
+
+
+	if ((sock_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP)) == -1) {
+		perror("socket");
+	}
+	if (sendto(sock_fd, &icmp_frame, sizeof(icmp_frame), 0, (struct sockaddr*)&ping_sockaddr, sizeof(ping_sockaddr)) < 0) {
+		perror("sendto");
+	}
+
 }
